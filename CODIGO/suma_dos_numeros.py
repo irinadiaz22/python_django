732326c765e1b6af9e7fcbcf3b5bb0f64964e14f
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-07-24 18:18:33 +0200

    lunes_18_18

---
+++ CODIGO/suma_dos_numeros.py
@@ -0,0 +1,13 @@
+"""
+Leer dos numeros de teclado y sumarlos
+"""
+
+# Lee de un número como texto y lo convierte a entero
+numero1 = int(input('Teclear numero entero:'))
+numero2 = int(input("Teclear otro numero entero: "))
+print(numero1 + numero2)
+
+# Lee de un número como texto y lo convierte a float
+numero1 = float(input('Teclear numero:'))
+numero2 = float(input("Teclear otro numero: "))
+print(numero1 + numero2)
