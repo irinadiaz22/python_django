fcde99ccb4eb011b34e57410c8befb17c83ae179
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-07-24 18:17:42 +0200

    lunes_18_17

---
+++ tipos_basicos.py
@@ -0,0 +1,16 @@
+"""
+Pruebas con tipos básicos: 
+numeros: int, float, complex
+"""
+
+numero = 1000
+print(numero, type(numero))
+
+numero = 5.3
+print(numero, type(numero))
+
+numero2 = "34"
+print(numero2, type(numero2))
+
+suma = numero + numero2
+print('mensaje')
