c572d7655be01aff9f885bc1dd589e3cb5c292fd
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-07-27 17:47:15 +0200

    jueves_17_46

---
+++ CODIGO/excepciones.py
@@ -0,0 +1,53 @@
+"""
+Captura de excepciones en Python
+"""
+
+def test1():
+    L = [1,2,3]
+    try:
+        print(L)
+        #print(10 / 0) Aborta el script porque no captura ZeroDivisionError
+        print(L[10])
+        print(L)
+    except IndexError as e:
+        print("ERROR",e)
+
+def test2():
+    # Capturar varios tipos de excepción
+    L = [1,2,3]
+    try:
+        print(L)
+        print(10 / 0) 
+        print(L[10])
+        print(L)
+    except IndexError as e:
+        print("ERROR",e)
+
+    except ZeroDivisionError as e:
+        print('ERROR', e)
+
+def test3():
+    # Capturar varios tipos de excepción y agruparlas
+    L = [1,2,3]
+    try:
+        print(L)
+        print(L[10])
+        print(10 / 0)         
+        print(L)
+    except (IndexError,ZeroDivisionError) as e:
+        # Para imprimir el nombre de la clase utilizamos la prop. __class__
+        print("ERROR",e.__class__.__name__, e)
+
+
+def test4():
+    # Capturar agrupando con la super clase: Exception
+    L = [1,2,3]
+    try:
+        print(L)
+        print(L[10])
+        print(10 / 0)         
+        print(L)
+    except Exception as e:
+        print("ERROR",e.__class__.__name__, e)
+       
+test4()
